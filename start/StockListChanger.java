62ccc190d2bd9f28342610614470593ad68126c3
Cory Kleiser <kleisecj@mail.uc.edu>
2016-08-02 19:50:40 -0400

    fixed loop error

--- start/StockListChanger.java
+++ start/StockListChanger.java
@@ -27,72 +27,77 @@ public class StockListChanger {
     private XPath path;
     
     public StockListChanger(){
+
+    }
+    
+    public void addNewList(){
+            
         try{
             DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
             builder = documentBuilderFactory.newDocumentBuilder();
             doc = builder.parse("src/com/test/itemsEX.xml");
             Element root = doc.getDocumentElement();
-        }
-        catch(ParserConfigurationException | SAXException | IOException e){
-            System.err.println(e);
-        }
-    }
-    
-    public void addNewList(){
+
         
-        Scanner in = new Scanner(System.in);
+            Scanner in = new Scanner(System.in);
         
-        Element list = doc.createElement("list");
-        System.out.print("Please enter a title or category for the new list:   ");
-        String category = in.nextLine();
-        Element cat = doc.createElement("cat");
-        cat.appendChild(doc.createTextNode(category));
-        list.appendChild(cat);
+            Element list = doc.createElement("list");
+            System.out.print("Please enter a title or category for the new list:   ");
+            String category = in.nextLine();
+            Element cat = doc.createElement("cat");
+            cat.appendChild(doc.createTextNode(category));
+            list.appendChild(cat);
 
-        StockList newList = new StockList(category);
+            StockList newList = new StockList(category);
 
-        boolean edit = true;
-        while (edit){
-            System.out.print("Please enter the ticker of the stock you would like to add:   ");
-            String ticker = in.next();
-            Element tik = doc.createElement("ticker");
-            tik.appendChild(doc.createTextNode(ticker));
-            list.appendChild(tik);
+            boolean edit = true;
+            while (edit){
+                    System.out.print("Please enter the ticker of the stock you would like to add:   ");
+                String ticker = in.next();
+                Element tik = doc.createElement("ticker");
+                tik.appendChild(doc.createTextNode(ticker));
+                list.appendChild(tik);
             
             
-            System.out.print("Please enter a rank you would like to associate with the stock:   ");
-            int rank = in.nextInt();
-            Element r = doc.createElement("rank");
-            r.appendChild(doc.createTextNode(Integer.toString(rank)));
-            list.appendChild(r);
+                System.out.print("Please enter a rank you would like to associate with the stock:   ");
+                int rank = in.nextInt();
+                Element r = doc.createElement("rank");
+                r.appendChild(doc.createTextNode(Integer.toString(rank)));
+                list.appendChild(r);
             
-            newList.addPick(ticker, rank);
+                newList.addPick(ticker, rank);
             
-            Element p = doc.createElement("lastUpdatedPrice");
+                Element p = doc.createElement("lastUpdatedPrice");
 
-            //TODO:: Create and add price: 
+//TODO:: Create and add price: 
                 //Maybe add function then call to update all prices on list.
                 //Maybe add i int at beginning of while to acess array then convert and add to xml.
             
 //            p.appendChild(doc.createTextNode());
             
-            System.out.println("Would you like to add another stock to your new list? (Y/N)   ");
-            String response = in.next();
+                System.out.println("Would you like to add another stock to your new list? (Y/N)   ");
+                String response = in.next();
+            
+                root.appendChild(list);
 
-            while(!response.equals("N")|!response.equals("Y")){
-                System.out.println("     !! INVALID INPUT :: Please Enter Y or N");
-                response = in.next();
-                System.out.println(response);
-            }            
+                while(!response.equals("N")&!response.equals("Y")){
+                    System.out.println("     !! INVALID INPUT :: Please Enter Y or N");
+                    response = in.next();
+                    System.out.println(response);
+                }            
 
-            if(response.equals("Y")){
-                continue;
+                if(response.equals("Y")){
+                    continue;
+                }
+
+                else{
+                    System.out.println(response);
+                    edit = false;
+                }
             }
-
-            else{
-                System.out.println(response);
-                edit = false;
-            }
+        }
+        catch(ParserConfigurationException | SAXException | IOException e){
+            System.err.println(e);
         }
     }
 }
