cac965a65d2e466d38f0458ef6057b3be86a259a
Cory Kleiser <kleisecj@mail.uc.edu>
2016-08-04 10:27:00 -0400

    routine commit

--- test/StockTester.java
+++ test/StockTester.java
@@ -27,14 +27,14 @@ public class StockTester {
         StockListParser parser = new StockListParser();  
         yourLists = parser.parse("src/com/test/itemsEX.xml");
         StockListChanger changer = new StockListChanger();
-        String userInput;
-        ArrayList<Pick> userPickSelection;
+        int userInput;
+        ArrayList<Pick> userPickSelection = new ArrayList<>();
         Scanner in = new Scanner(System.in);
         int i=0;
         String optionsPrompt = "#Options  ::\n"
-                + "     view POS_ON_LIST - displays specified list (specified by list position starting at 1)"
+                + "     view POS_ON_LIST - displays specified list (specified by list position starting at 1)\n"
                 + "     add list - adds new StockList object with desired picks\n"
-                + "     rm list - deletes StockList object from your Portfolios"
+                + "     rm list - deletes StockList object from your Portfolios\n"
                 + "     add pick - adds new pick to your list"
                 + "     rm pick - deletes pick from your list"
                 + "     help - displays this help menu"
@@ -48,12 +48,12 @@ public class StockTester {
         System.out.println("\nHello");
         
         //starts
-        boolean finished = false;
-        while(!finished){
+
             System.out.println(optionsPrompt);
 
         
-        
+        boolean finished = false;
+        while(!finished){
             for(StockList aList : yourLists){
                 i++;
                 System.out.println("List "+i+")     "+aList.getListCategory());
@@ -62,34 +62,57 @@ public class StockTester {
 
 
             //TODO:: catch user error
-
-            boolean valid = false;
+            System.out.println("You can enter -1 to add a list ");
+//            System.out.println("Or enter the list number to view an existing list");
+                boolean valid=false;
                 while(!valid){
                     try{
-                        System.out.println("\nType help if you need some:");
-                        userInput = in.next();
-                        handleInput(userInput);
-//                        if(userInput){
-//                            System.err.print(invalidSelection);
-//                            continue;
-//                        }
-//                        else{
-//                            userInput = userInput-1;
-//
-//                            userPickSelection = yourLists.get(userInput).getPicks();
-//                            for(Pick pick : userPickSelection){
-//                                i++;
-//                                System.out.print("\nPick "+i+")     "+pick.getTicker()+"       Price: "+pick.getSharePrice()+" "+pick.getStock().getCurrency());
-//                            }
-                        valid=true;
+                        System.out.print("If you would view a list enter the line number:    ");
+                        userInput = in.nextInt();
+                        if(userInput<1|userInput>yourLists.size()){
+                            if (userInput == -1){
+                                changer.addNewList();
+                            }
+                            continue;
+                        }
+                        else{
+                            userInput = userInput-1;
+
+                            userPickSelection = yourLists.get(userInput).getPicks();
+                            for(Pick pick : userPickSelection){
+                                i++;
+                                System.out.print("\nPick "+i+")     "+pick.getTicker()+"       Price: "+pick.getSharePrice()+" "+pick.getStock().getCurrency()+"\n");
+                            }
+                            i=0;
+                            valid=true;
+                        }
+
+
                     }
-                    
-                    
                     catch(InputMismatchException e){
                       System.err.println(invalidSelection);
                       in.nextLine();
                     }
-                }
+                    try{
+                        System.out.print("Enter the stock's line number to see more details");
+                        userInput = in.nextInt();
+                        if(userInput<0|userInput>userPickSelection.size()){
+                            System.out.print(userPickSelection.get(userInput-1).format());
+                        }
+                    }
+                    catch(InputMismatchException e){
+                        System.err.println(invalidSelection);
+                        in.nextLine();
+                    }
+                    try{
+                    System.out.print("Enter 0 to quit/Enter anything to continue your lists");
+                    userInput = in.nextInt();
+                    if (userInput == 0);
+                        return;
+                    }
+                    catch(InputMismatchException e){
+                        in.nextLine();
+                    }
             }
 
             
@@ -109,74 +132,74 @@ public class StockTester {
         
         
     }
-    private static void handleInput(String re){
-        Scanner in = new Scanner(System.in);
-                //add commands to list
-        ArrayList<String> commands = new ArrayList<>();
-        commands.add("add list");
-        commands.add("add pick");
-        commands.add("rm list");
-        commands.add("rm pick");
-        commands.add("view");
-        
-        //create changer to alter list contents
-        StockListChanger changer = new StockListChanger();
-        
-        
-        boolean valid = false;
-        while(!false){
-            while(re.equals("add list")){
-                changer.addNewList();
-            }
-            while(re.equals("rm list")){
-                System.out.println("Enter list to be removed:     ");
-                re = in.next();
-                changer.removeList(re);
-            }
-            while(re.equals("add pick")){
-//                System.out.print("Enter your new Portforlio name:     ");
+//    private static void handleInput(String re){
+//        Scanner in = new Scanner(System.in);
+//                //add commands to list
+//        ArrayList<String> commands = new ArrayList<>();
+//        commands.add("add list");
+//        commands.add("add pick");
+//        commands.add("rm list");
+//        commands.add("rm pick");
+//        commands.add("view");
+//        
+//        //create changer to alter list contents
+//        StockListChanger changer = new StockListChanger();
+//        
+//        
+//        boolean valid = false;
+//        while(!false){
+//            while(re.equals("add list")){
+//                changer.addNewList();
+//            }
+//            while(re.equals("rm list")){
+//                System.out.println("Enter list to be removed:     ");
 //                re = in.next();
-//                changer.addToList(re);
-            }
-            while(re.equals("rm pick")){
-                
-            }
-            while(re.equals("view")){
-                
-                System.out.println("Please Enter List Number");
-                in.nextLine();
-                int place = in.nextInt();
-
-                
-                StockListParser parser = new StockListParser();
-                
-                place = place-1;
-
-                StockList userSelection = parser.getStockList(place);
-                
-                
-                if(userSelection == null){
-                    System.err.println("Try Again");
-                    continue;
-                }
-                else{
-
-                    ArrayList<Pick> userPickSelection = parser.getStockList(place).getPicks();
-                    int i = 0;
-                    for(Pick pick : userPickSelection){
-                        i++;
-                        System.out.println("\nPick "+i+")     "+pick.getTicker()+"       Price: "+pick.getSharePrice()+" "+pick.getStock().getCurrency());
-                    }
-
-            }
-            while(re.equals("help")){
-                
-            }
-               
-        System.out.println("'"+re+"' is not a valid command\n          enter HELP to see a list of commands.");        
-            
-        }
-    }
+//                changer.removeList(re);
+//            }
+//            while(re.equals("add pick")){
+////                System.out.print("Enter your new Portforlio name:     ");
+////                re = in.next();
+////                changer.addToList(re);
+//            }
+//            while(re.equals("rm pick")){
+//                
+//            }
+//            while(re.equals("view")){
+//                
+//                System.out.println("Please Enter List Number");
+//                in.nextLine();
+//                int place = in.nextInt();
+//
+//                
+//                StockListParser parser = new StockListParser();
+//                
+//                place = place-1;
+//
+//                StockList userSelection = parser.getStockList(place);
+//                
+//                
+//                if(userSelection == null){
+//                    System.err.println("Try Again");
+//                    continue;
+//                }
+//                else{
+//
+//                    ArrayList<Pick> userPickSelection = parser.getStockList(place).getPicks();
+//                    int i = 0;
+//                    for(Pick pick : userPickSelection){
+//                        i++;
+//                        System.out.println("\nPick "+i+")     "+pick.getTicker()+"       Price: "+pick.getSharePrice()+" "+pick.getStock().getCurrency());
+//                    }
+//
+//            }
+//            while(re.equals("help")){
+//                
+//            }
+//               
+//        System.out.println("'"+re+"' is not a valid command\n          enter -5 to see a list of commands.");        
+//            
+//        }
+//    }
 }
 }
 
